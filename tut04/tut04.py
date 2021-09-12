5af31f581bb58ee88a24290ac1ba17d1a037c7b1
Ashutosh <akashutosh82@gmail.com>
2021-09-13 02:42:59 +0530

    output file

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import os
 import csv
 from openpyxl import Workbook
@@ -67,74 +66,3 @@ def output_individual_roll():
  
 output_by_subject()
 output_individual_roll()
-||||||| constructed merge base
-=======
-import os
-import csv
-from openpyxl import Workbook
-from openpyxl import load_workbook
-
- 
-def output_by_subject():
-    FOLDER = "output_by_subject"
-    header = ["rollno","register_sem","subno","sub_type"]
-    sub_list = []
-    
-    if not os.path.exists(FOLDER): 
-        os.makedirs(FOLDER)
-    with open('regtable_old.csv','r') as file:
-        stud_list=csv.reader(file)
-        for data1 in stud_list:
-            data= []
-            data.append(data1[0])
-            data.append(data1[1])
-            data.append(data1[3])
-            data.append(data1[8])
-            if (data[2] =="subno"):continue
-            if data[2] not in sub_list:
-             sub_list.append(data[2])
-             wb=Workbook()
-             sheet=wb.active
-             sheet.append(header)
-             sheet.append(data)
-             wb.save(f'output_by_subject\\{data[2]}.xlsx')
-            else:
-              wb=load_workbook(r'output_by_subject\\{}.xlsx'.format(data[2]))
-              sheet=wb.active
-              sheet.append(data)
-              wb.save(f'output_by_subject\\{data[2]}.xlsx')
-    return
- 
-def output_individual_roll():
-    FOLDER = "output_individual_roll"
-    header = ["rollno","register_sem","subno","sub_type"]
-    roll_list = []
-    
-    if not os.path.exists(FOLDER): 
-        os.makedirs(FOLDER)
-    with open('regtable_old.csv','r') as file:
-        stud_list=csv.reader(file)
-        for data1 in stud_list:
-            data= []
-            data.append(data1[0])
-            data.append(data1[1])
-            data.append(data1[3])
-            data.append(data1[8])
-            if (data[0] =="rollno"):continue
-            if data[0] not in roll_list:
-             roll_list.append(data[0])
-             wb=Workbook()
-             sheet=wb.active
-             sheet.append(header)
-             sheet.append(data)
-             wb.save(f'output_individual_roll\\{data[0]}.xlsx')
-            else:
-              wb=load_workbook(r'output_individual_roll\\{}.xlsx'.format(data[0]))
-              sheet=wb.active
-              sheet.append(data)
-              wb.save(f'output_individual_roll\\{data[0]}.xlsx')
-    return
- 
-output_by_subject()
-output_individual_roll()
->>>>>>> Stashed changes
